fb60b62e2834c94232bd2653e9c1ebdc6ce97a81
kizhore15 <kishorey3260@gmail.com>
2025-01-12 10:09:27 +0530

    third commit

--- cypress/e2e/api testing/basic.cy.js
+++ cypress/e2e/api testing/basic.cy.js
@@ -1,20 +1,20 @@
-describe("http request",()=>{
-    it("get method",()=>{
-        cy.request('GET', 'https://jsonplaceholder.typicode.com/posts/1')
-        .its('status').should('equal', 200)
-    })
+// describe("http request",()=>{
+//     it("get method",()=>{
+//         cy.request('GET', 'https://jsonplaceholder.typicode.com/posts/1')
+//         .its('status').should('equal', 200)
+//     })
 
-    it("put method",()=>{
-        cy.request({
-            method:"POST",
-            url:"https://jsonplaceholder.typicode.com/posts",
-            body:{
-                    name: "id labore ex et quam laborum",
-                    email: "Eliseo@gardner.biz",
-                    body: "laudantium enim quasi est accusantium"
-            }
+//     it("put method",()=>{
+//         cy.request({
+//             method:"POST",
+//             url:"https://jsonplaceholder.typicode.com/posts",
+//             body:{
+//                     name: "id labore ex et quam laborum",
+//                     email: "Eliseo@gardner.biz",
+//                     body: "laudantium enim quasi est accusantium"
+//             }
 
-        })
-        .its('status').should('equal', 201)
-    })
-})
+//         })
+//         .its('status').should('equal', 201)
+//     })
+// })
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,24 +1,24 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://practicetestautomation.com/practice-test-login/')
-//     cy.get('input#username').type('student')
-//     cy.get('input#password').type('Password123')
-//     cy.get('.btn').click()
-//     cy.url().should('include', 'practicetestautomation.com/logged-in-successfully')
-//     cy.get('strong').should('be.visible','Congratulations student. You successfully logged in!')
-//     cy.get('a[href*="practice-test-login"]').click()
-//     cy.get(4000)
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://practicetestautomation.com/practice-test-login/')
+    cy.get('input#username').type('student')
+    cy.get('input#password').type('Password123')
+    cy.get('.btn').click()
+    cy.url().should('include', 'practicetestautomation.com/logged-in-successfully')
+    cy.get('strong').should('be.visible','Congratulations student. You successfully logged in!')
+    cy.get('a[href*="practice-test-login"]').click()
+    cy.get(4000)
 
-//   })
+  })
 
-//   it('verify incorrect username',()=>{
-//     cy.visit('https://practicetestautomation.com/practice-test-login/')
-//     cy.get('input#username').type(' incorrectUser')
-//     cy.get('input#password').type('Password123')
-//     cy.get('.btn').click()
-//     cy.get('div#error').should('be.visible').and('have.text','Your username is invalid!')
-//   })
-// })
+  it('verify incorrect username',()=>{
+    cy.visit('https://practicetestautomation.com/practice-test-login/')
+    cy.get('input#username').type(' incorrectUser')
+    cy.get('input#password').type('Password123')
+    cy.get('.btn').click()
+    cy.get('div#error').should('be.visible').and('have.text','Your username is invalid!')
+  })
+})
 
 
 // describe('Practice Test Automation',()=>{
@@ -63,19 +63,19 @@
 
 //   excel data to json
 
-describe('Read data from Excel file', () => {
-  it('should read data from Excel', () => {
-    cy.visit('https://practicetestautomation.com/practice-test-login/')
+// describe('Read data from Excel file', () => {
+//   it('should read data from Excel', () => {
+//     cy.visit('https://practicetestautomation.com/practice-test-login/')
 
-    cy.task('readExcel', {
-      filePath: 'C:/Users/kishore kumar/OneDrive/Desktop/cypress/cypress/fixtures/login.xlsx',  // Update with actual file path
-      sheetName: 'loginreg'  // Specify the sheet name
-    }).then((data) => {
-      cy.log(JSON.stringify(data));
-      // expect(data[0].Name).to.equal('John Doe'); // Example validation
-      cy.get('input#username').type(data[0].Username)
-      cy.get('input#password').type(data[0].Password)
-      cy.get('.btn').click()
-    });
-  });
-});
+//     cy.task('readExcel', {
+//       filePath: 'C:/Users/kishore kumar/OneDrive/Desktop/cypress/cypress/fixtures/login.xlsx',  // Update with actual file path
+//       sheetName: 'loginreg'  // Specify the sheet name
+//     }).then((data) => {
+//       cy.log(JSON.stringify(data));
+//       // expect(data[0].Name).to.equal('John Doe'); // Example validation
+//       cy.get('input#username').type(data[0].Username)
+//       cy.get('input#password').type(data[0].Password)
+//       cy.get('.btn').click()
+//     });
+//   });
+// });
